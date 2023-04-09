003e2fbf7376acc978932540de1dda5124aa84f4
Anamaxal07 <neilbryanfranco02@gmail.com>
2023-04-09 19:15:06 +0800

    my third commit

--- PHP/toys.php
+++ PHP/toys.php
@@ -1,12 +1,54 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+$TOYS = array(
+    array(
+        "toy_category" => "Arts and Crafts",
+        "toy_name" => "Play-Doh",
+        "toy_price" => "₱500",
+        "toy_stocks" => "20",
+        "toy_material" => "Plastic",
+    ),
 
-    header('Access-Control-Allow-Origin: *');
-    $myObj = new stdclass();
-    $myObj->name = "John";
-    $myObj->age = 30;
-    $myObj->city = "New York";
+    array(
+        "toy_category" => "Building Blocks",
+        "toy_name" => "Mega Bloks",
+        "toy_price" => "₱700",
+        "toy_stocks" => "10",
+        "toy_material" => "Plastic",
+    ),
 
-    $myJSON = json_encode($myObj); 
+    array(
+        "toy_category" => "Educational Toys",
+        "toy_name" => "STEM Solar Robots",
+        "toy_price" => "₱2,500",
+        "toy_stocks" => "50",
+        "toy_material" => "Metal",
+    ),
 
-    echo $myJSON;
+    array(
+        "toy_category" => "Stuffed Animals",
+        "toy_name" => "Teddy bears",
+        "toy_price" => "₱600",
+        "toy_stocks" => "17",
+        "toy_material" => "Fabric",
+    ),
+
+    array(
+        "toy_category" => "Board Games",
+        "toy_name" => "Chess",
+        "toy_price" => "₱499",
+        "toy_stocks" => "30",
+        "toy_material" => "Wood",
+    ),
+
+    array(
+        "toy_category" => "Action Figures",
+        "toy_name" => "Transformers",
+        "toy_price" => "₱800",
+        "toy_stocks" => "20",
+        "toy_material" => "Metal",
+    ),
+);
+echo json_encode(array("toys" => $TOYS));
+echo $data;
 ?>
